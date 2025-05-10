d78155e9a565e9dd6daaebf0ab69a8da97974008
Tanel Pauls <paulstanel@protonmail.com>
2025-05-10 22:32:48 +0300

    changed database tables.

--- init_tables.php
+++ init_tables.php
@@ -6,34 +6,50 @@ DROP TABLE IF EXISTS KUSIMUSED;
 ";
 
 if (!$mysqli->query($sql)) {
-    error_log("Error creating KUSIMUSED table: " . $mysqli->error);
+    error_log("Error deleting table: KUSIMUSED." . $mysqli->error);
 }
+
+$sql = "
+DROP TABLE IF EXISTS HAALETAJAD;
+";
+
+if (!$mysqli->query($sql)) {
+    error_log("Error deleting table: HAALETAJAD." . $mysqli->error);
+}
+
+
 $sql = "
 DROP TABLE IF EXISTS HAALETUS;
 ";
 
 if (!$mysqli->query($sql)) {
-    error_log("Error creating HAALETUS table: " . $mysqli->error);
+    error_log("Error deleting table: HAALETUS." . $mysqli->error);
 }
+
+
 $sql = "
 DROP TABLE IF EXISTS TULEMUSED;
 ";
 
 if (!$mysqli->query($sql)) {
-    error_log("Error creating TULEMUSED table: " . $mysqli->error);
+    error_log("Error deleting table: TULEMUS." . $mysqli->error);
 }
+
+
 $sql = "
 DROP TABLE IF EXISTS LOGI;
 ";
 
 if (!$mysqli->query($sql)) {
-    error_log("Error creating LOGI table: " . $mysqli->error);
+    error_log("Error deleting table: LOGI." . $mysqli->error);
 }
+
 
 $sql = "
 CREATE TABLE IF NOT EXISTS KUSIMUSED (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Kusimus VARCHAR(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+    Kusimus_id INT AUTO_INCREMENT PRIMARY KEY,
+    Kusimus VARCHAR(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    H_alguse_aeg DATETIME NOT NULL
 ) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ";
 
@@ -42,12 +58,26 @@ if (!$mysqli->query($sql)) {
 }
 
 $sql = "
+CREATE TABLE IF NOT EXISTS HAALETAJAD (
+    Haaletaja_id INT AUTO_INCREMENT PRIMARY KEY,
+    Eesnimi VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+    Perenimi VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+";
+
+if (!$mysqli->query($sql)) {
+    error_log("Error creating HAALETAJAD table: " . $mysqli->error);
+}
+
+$sql = "
 CREATE TABLE IF NOT EXISTS HAALETUS (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    Eesnimi VARCHAR(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    Perenimi VARCHAR(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    Haaletuse_aeg TIME NOT NULL,
-    Otsus INT NOT NULL
+    Haaletaja_id INT NOT NULL,
+    Kusimus_id INT NOT NULL,
+    Haaletuse_aeg DATETIME NOT NULL,
+    Otsus ENUM('poolt', 'vastu') NOT NULL,
+    FOREIGN KEY (Haaletaja_id) REFERENCES HAALETAJAD(Haaletaja_id) ON DELETE CASCADE,
+    FOREIGN KEY (Kusimus_id) REFERENCES KUSIMUSED(Kusimus_id) ON DELETE CASCADE
 ) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ";
 
@@ -57,10 +87,12 @@ if (!$mysqli->query($sql)) {
 
 $sql = "
 CREATE TABLE IF NOT EXISTS TULEMUSED (
-    Haaletajate_arv INT,
-    H_alguse_aeg TIME NOT NULL,
+    Kusimus_id INT PRIMARY KEY,
+    Haaletajate_arv INT NOT NULL,    
+    H_alguse_aeg DATETIME NOT NULL,
     Poolt INT,
-    Vastu INT
+    Vastu INT,
+    FOREIGN KEY (Kusimus_id) REFERENCES KUSIMUSED(Kusimus_id) ON DELETE CASCADE
 ) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ";
 
@@ -70,10 +102,14 @@ if (!$mysqli->query($sql)) {
 
 $sql = "
 CREATE TABLE IF NOT EXISTS LOGI (
-    Haaletaja_id INT,
-    H_alguse_aeg TIME NOT NULL,
-    Haale_andmise_aeg TIME NOT NULL,
-    Haale_suund INT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Haaletaja_id INT NOT NULL,
+    Kusimus_id INT NOT NULL,
+    H_alguse_aeg DATETIME NOT NULL,
+    Haale_andmise_aeg DATETIME NOT NULL,
+    Haale_suund ENUM('poolt', 'vastu') NOT NULL,
+    FOREIGN KEY (Haaletaja_id) REFERENCES HAALETAJAD(Haaletaja_id),
+    FOREIGN KEY (Kusimus_id) REFERENCES KUSIMUSED(Kusimus_id)
 ) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 ";
 
