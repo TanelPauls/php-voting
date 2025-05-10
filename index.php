cca131f84d0c11874b8066da7a936a3df2887b90
Tanel Pauls <paulstanel@protonmail.com>
2025-05-10 07:13:09 +0300

    Added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Include the database connection file
+include_once("config.php");
+
+// Fetch contacts (in descending order)
+// $result = mysqli_query($mysqli, "SELECT * FROM ISIKUD ORDER BY Isiku_id DESC"); 
+?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>MariaDB Isikud andmed</title>
+	<link rel="stylesheet" href="styles.css" />
+	
+</head>
+<body>
+	Hello World!
+</body>
+</html>
